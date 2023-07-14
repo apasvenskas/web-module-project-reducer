0d283f4cbf03c03aed3c211c1e40f372ba2c54cd
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-07-14 15:47:48 -0700

    state operations

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-
+import React, { useReducer } from 'react';
+import reducer, { initialState } from '../reducers/index.js';
 import './App.css';
 
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
+
+
 function App() {
+  const [ state, dispatch ] = useReducer(reducer, initialState);
+
+  console.log(state);
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
